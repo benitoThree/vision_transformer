85d9b67903b1c2670617c9f856f632084e3918e4
Benito Geordie <benito@thirdai.com>
2022-01-21 01:28:14 -0600

    embeddings

--- experiments/get_pretrained_mixer_embeddings_all.sh
+++ experiments/get_pretrained_mixer_embeddings_all.sh
@@ -1,4 +1,1 @@
-python3 /home/benito/vision_transformer/keras/mlp_mixer_imagenet_pretrain_embeddings.py \ 
-    /home/benito/vision_transformer/pretrained_params/imagenet21k-Mixer-B_16.npy \
-    /media/scratch/data/birds/train/ \
-    /home/benito/vision_transformer/experiments/pretrained_mixer_embeddings_all.npy 
+python3 /home/benito/vision_transformer/keras/mlp_mixer_imagenet_pretrain_embeddings.py /home/benito/vision_transformer/pretrained_params/imagenet21k-Mixer-B_16.npy /media/scratch/data/birds/train/ /home/benito/vision_transformer/experiments/pretrained_mixer_embeddings_all.npy 
