f7f513b20708598604bfcf130ca10791b3c75b4f
Benito Geordie <benito@thirdai.com>
2022-01-21 01:38:06 -0600

    debug

--- keras/mlp_mixer_imagenet_pretrain_embeddings.py
+++ keras/mlp_mixer_imagenet_pretrain_embeddings.py
@@ -182,14 +182,14 @@ for batch in train_data:
     x = (batch[0] - 127.5) / 127.5
     new_output = model(x)
     print(new_output.shape)
-    np.append(outputs, new_output, 0)
+    outputs = np.append(outputs, new_output, 0)
     print(outputs.shape)
 
 for batch in datagen.flow(x_train, y_train, batch_size=batch_size):
     x = (batch[0] - 127.5) / 127.5
     new_output = model(x)
     print(new_output.shape)
-    np.append(outputs, new_output, 0)
+    outputs = np.append(outputs, new_output, 0)
     print(outputs.shape)
 
 np.save(npy_save_file, outputs)
